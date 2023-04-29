82acc41a37f6f35ea79eef4e02b98bded8c2097d
vanipathri <vani.pathri@gmail.com>
2023-04-29 08:00:28 -0700

    Its even and odd

---
+++ evenodd.java
@@ -0,0 +1,12 @@
+public class evenodd {
+    public static  void checkevenodd(int x){
+        if(x%2==0){
+            System.out.println("Even");
+        } else {
+            System.out.println("odd");
+        }
+    }
+    public static void main(String [] args){
+        checkevenodd(5);
+    }
+}
