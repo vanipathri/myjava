4ce6ef6ccf0aa0abfacd03068dbffd73e1e706be
vanipathri <131564884+vanipathri@users.noreply.github.com>
2023-04-30 21:02:46 -0700

    Add files via upload

---
+++ evenodd.java
@@ -0,0 +1,16 @@
+public class evenodd{
+public static void main(String[] args){
+    evenodd(23);
+}
+  public static void evenodd( int x ){
+    if (x%2==0){
+        System.out.println("number is even");
+    }
+
+
+    else {
+        System.out.println("number is odd");
+    }
+  }
+
+}
