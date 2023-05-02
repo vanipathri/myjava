88ea15418668622b4fcb2508df50d224d4c5ee87
vanipathri <131564884+vanipathri@users.noreply.github.com>
2023-05-01 18:08:51 -0700

    Add files via upload

---
+++ table7.java
@@ -0,0 +1,10 @@
+-public class table7{
+    public static void main( String[]args){
+  printtable(7,9);
+    }
+    public static void printtable(int x, int y)
+
+    {
+        for (int i = 0; i <= 5; i++)
+            System.out.println(x + "*" + i + "=" + x * i);
+    }}
